83a67ec80d3a9d629980a4505bc486deb9f5090a
Natoonka <natalkakostecka@gmail.com>
2022-05-24 11:22:39 +0200

    Added assets

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items")]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite icon;
+}
